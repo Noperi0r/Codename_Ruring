480be8c8181454dbce1794d96cd521dd9ddd8d88
Noperi0r <mathewgwon@gmail.com>
2024-07-14 07:20:16 +0900

    [Add] Intro scene

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+    [SerializeField] GameObject _tutorialPicParent;
+    int _totalPageNum;
+    int _curPageNum;
+
+    void Start()
+    {
+        _totalPageNum = _tutorialPicParent.transform.childCount;
+        _curPageNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            _tutorialPicParent.transform.GetChild(_curPageNum++)?.gameObject.SetActive(false);
+            if(_curPageNum >= _totalPageNum) 
+            {
+                Debug.Log("tutorial enD");
+                // TODO: 튜토리얼 종료 이벤트
+            }
+        }
+    }
+}
